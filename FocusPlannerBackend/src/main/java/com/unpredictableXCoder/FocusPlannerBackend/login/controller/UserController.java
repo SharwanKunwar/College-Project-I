1b42ea8d72bc2356917c76b873a0e9b4c7a4dc6e
SharwanKunwar <sharwankunwar07@gmail.com>
2026-07-12 11:57:31 +0545

    completed service and controller

--- FocusPlannerBackend/src/main/java/com/unpredictableXCoder/FocusPlannerBackend/login/controller/UserController.java
+++ FocusPlannerBackend/src/main/java/com/unpredictableXCoder/FocusPlannerBackend/login/controller/UserController.java
@@ -5,8 +5,7 @@ import com.unpredictableXCoder.FocusPlannerBackend.login.service.UserServiceHelp
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,17 +15,46 @@ public class UserController {
     private final UserServiceHelper  userServiceHelper;
 
     // post user
-    public ResponseEntity<UserDTO> createUser(UserDTO userDTO)
+    @PostMapping
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO)
     {
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(userServiceHelper.createUser(userDTO));
+        return ResponseEntity.status(HttpStatus.CREATED).body(userServiceHelper.createUser(userDTO));
     }
 
-    //Get all users
+    // get all users
+    @GetMapping("/all")
     public ResponseEntity<Iterable<UserDTO>> getAllUsers()
     {
-        return ResponseEntity
-                .ok(userServiceHelper.getAllUsers());
+        return ResponseEntity.ok(userServiceHelper.getAllUsers());
     }
+
+    //Get user by email
+    @GetMapping("/email/{email}")
+    public ResponseEntity<UserDTO> getUserByEmail(@PathVariable String email)
+    {
+        return ResponseEntity.ok(userServiceHelper.getUserByEmail(email));
+    }
+
+    // Delete user
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<String> deleteUser(@PathVariable("userId") String userId)
+    {
+        userServiceHelper.deleteUser(userId);
+        return ResponseEntity.ok("User deleted successfully.");
+    }
+
+    //Update user
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable("userId") String userId, @RequestBody UserDTO userDTO)
+    {
+        return ResponseEntity.ok(userServiceHelper.updateUser(userDTO, userId));
+    }
+
+    //Get user by id
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable("userId") String userId)
+    {
+        return ResponseEntity.ok(userServiceHelper.getUserById(userId));
+    }
+
 }
--- FocusPlannerBackend/src/main/java/com/unpredictableXCoder/FocusPlannerBackend/login/service/imps/UserServiceIMP.java
+++ FocusPlannerBackend/src/main/java/com/unpredictableXCoder/FocusPlannerBackend/login/service/imps/UserServiceIMP.java
@@ -1,15 +1,19 @@
 package com.unpredictableXCoder.FocusPlannerBackend.login.service.imps;
 
 import com.unpredictableXCoder.FocusPlannerBackend.exceptions.BadRequestException;
+import com.unpredictableXCoder.FocusPlannerBackend.exceptions.ResourceNotFoundException;
 import com.unpredictableXCoder.FocusPlannerBackend.exceptions.UserAlreadyExistsException;
 import com.unpredictableXCoder.FocusPlannerBackend.login.dtos.UserDTO;
 import com.unpredictableXCoder.FocusPlannerBackend.login.entities.Provider;
 import com.unpredictableXCoder.FocusPlannerBackend.login.entities.User;
+import com.unpredictableXCoder.FocusPlannerBackend.login.helper.UserHelper;
 import com.unpredictableXCoder.FocusPlannerBackend.login.repository.UserRepository;
 import com.unpredictableXCoder.FocusPlannerBackend.login.service.UserServiceHelper;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -41,24 +45,45 @@ public class UserServiceIMP implements UserServiceHelper {
     }
 
     @Override
-    public UserDTO getUserByEmail(String email) {
-        return null;
+    public UserDTO getUserByEmail(String email)
+    {
+        User user = userRepository.findByEmail(email).orElseThrow(()-> new ResourceNotFoundException("User not found with email: " + email));
+        User savedUser = modelMapper.map(user, User.class);
+        return modelMapper.map(savedUser, UserDTO.class);
     }
 
     @Override
-    public UserDTO updateUser(UserDTO userDTO, String userId) {
-        return null;
+    public UserDTO updateUser(UserDTO userDTO, String userId)
+    {
+        UUID uId = UUID.fromString(userId);
+        User existingUser = userRepository.findById(uId).orElseThrow(()-> new ResourceNotFoundException("User not found with id: " + uId));
+        // email must not be changed
+        if(userDTO.getName() != null) existingUser.setName(userDTO.getName());
+        //todo: change password update login... hashing
+        if(userDTO.getPassword() != null) existingUser.setPassword(userDTO.getPassword());
+        if(userDTO.getProvider() != null) existingUser.setProvider(userDTO.getProvider());
+        if(userDTO.getImage() != null) existingUser.setImage(userDTO.getImage());
+        existingUser.setEnabled(userDTO.isEnable());
+        User user = userRepository.save(existingUser);
+        return modelMapper.map(user, UserDTO.class);
     }
 
     @Override
-    public void deleteUser(String userId) {
-
+    public void deleteUser(String userId)
+    {
+        UUID uId = UserHelper.parseUUID(userId);
+        User user = userRepository.findById(uId).orElseThrow(()-> new ResourceNotFoundException("User not found with id: " + userId));
+        userRepository.delete(user);
     }
 
     @Override
-    public UserDTO getUserById(String id) {
-        return null;
+    public UserDTO getUserById(String id)
+    {
+        UUID uId = UserHelper.parseUUID(id);
+        User user = userRepository.findById(uId).orElseThrow(()-> new ResourceNotFoundException("User not found with id: " + id));
+        return modelMapper.map(user, UserDTO.class);
     }
+
 
 
 }
