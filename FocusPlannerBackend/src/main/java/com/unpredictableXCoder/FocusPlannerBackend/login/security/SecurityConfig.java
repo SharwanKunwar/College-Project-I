bad4b11cc0ff43b03416a0f1e5cbd411308c930d
SharwanKunwar <sharwankunwar07@gmail.com>
2026-07-13 12:54:28 +0545

    added userDetailService and passwordEncoder

--- FocusPlannerBackend/src/main/java/com/unpredictableXCoder/FocusPlannerBackend/login/security/SecurityConfig.java
+++ FocusPlannerBackend/src/main/java/com/unpredictableXCoder/FocusPlannerBackend/login/security/SecurityConfig.java
@@ -1,5 +1,34 @@
 package com.unpredictableXCoder.FocusPlannerBackend.login.security;
 
-public class SecurityConfig {
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig
+{
+    /*
+    @Bean
+    public UserDetailsService users()
+    {
+        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+        UserDetails user1 = userBuilder.username("sharwan").password("xyz").roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user1);
+    }
+    */
+
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+
+
 
 }
